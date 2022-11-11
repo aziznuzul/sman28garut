0ad55f5bca3a64e86f83d61c8d673a1e3126f718
Msyarif17 <m.syarifsetiadi@gmail.com>
2022-11-11 12:35:06 +0700

    first add

---
+++ resources/views/home/about.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.frontend.app',[
+    'title' => 'About',
+])
+@section('content')
+<div class="regular-page-area section-padding-100">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="page-content">
+                    <h4>About Us</h4>
+                    <p>TUJUAN SEKOLAH :
+                        <p>Tujuan sekolah sebagai bagian dari tujuan pendidikan nasional adalah meningkatkan kecerdasan, pengetahuan, kepribadian, akhlak mulia, serta keterampilan untuk hidup mandiri dan mengikuti pendidikan lebih lanjut. Secara lebih rinci tujuan SMA Negeri 28 Garut Kabupaten Garut Propinsi Jawa Barat adalah sebagai berikut :</p>   
+                            <p>
+                            <li>Menyediakan sarana prasarana pendidikan yang memadai;</li> 
+                            <li>Melaksanakan proses belajar mengajar secara efektif dan efisien, berdasarkan semangat keunggulan lokal dan global;</li>
+                            <li>Meningkatkan kinerja masing-masing komponen sekolah (Kepala sekolah, tenaga pendidik, karyawan, peserta didik, dan komite sekolah) untuk bersama-sama melaksanakan kegiatan yang inovatif sesuai dengan Tugas Pokok dan Fungsi (TUPOKSI) masing-masing;</li>
+                            <li>Meningkatkan program ekstrakurikuler dengan mewajibkan pramuka bagi seluruh warga, agar lebih efektif dan efisien sesuai dengan bakat dan minat peserta didik sebagai salah satu sarana pengembangan diri peserta didik;</li>
+                            <li>Mewujudkan peningkatkan kualitas lulusan yang memiliki sikap, pengetahuan, dan keterampilan yang seimbang, serta meningkatkan jumlah lulusan yang melanjutkan ke perguruan tinggi;</li>
+                            <li>Menyusun dan melaksanakan tata tertib dan segala ketentuan yang mengatur operasional warga sekolah;</li>
+                            <li>Meningkatkan kualitas semua Sumber Daya Manusia baik tenaga pendidik, tenaga kependidikan, dan peserta didik yang dapat berkompetisi baik lokal maupun global. </li>
+                            </p>
+                        <p>VISI : “Terwujudnya SMA yang BERBUDI ( Berilmu, berkarya, berbudaya, dan berprestasi) berlandaskan iman dan taqwa ”</p>
+                        <p>MISI :</p>
+                            <li>Melaksanakan pembelajaran yang efektif untuk mengembangkan daya pikir-kalbu-fisik secara optimal.
+                            <li>Melaksanakan pengayaan untuk mempersiapkan siswa yang unggul dalam kompetisi akademik.
+                            <li>Menumbuhkan dan mengembangkan pola pikir dan tindakan yang mencerminkan budaya mutu dan akhlaq mulia dalam kehidupan sehari-hari.
+                            <li>Melaksanakan pembinaan berbagai bidang olah raga sehingga siswa memiliki daya fisik yang sehat dan tangguh.
+                            <li>Menyelenggarakan dan memfasilitasi kegiatan yang mencerminkan pengembangan seni budaya bangsa.
+                            <li>Melaksanakan pendidikan kecakapan hidup guna menciptakan insan yang religius, mandiri, kreatif, dan kompetitif.
+                        </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
---
+++ resources/views/layouts/frontend/footer.blade.php
@@ -0,0 +1,33 @@
+<footer class="footer-area">
+    <!-- Top Footer Area -->
+    <div class="top-footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <!-- Footer Logo -->
+                    <div class="footer-logo">
+                        <a href="/"><h3 class="text-light">SMAN 28 GARUT</h3></a>
+                    </div>
+                    <!-- Copywrite -->
+                    <p><a href="#"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright SMAN 28 Garut &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Bottom Footer Area -->
+    <div class="bottom-footer-area d-flex justify-content-between align-items-center">
+        <!-- Contact Info -->
+        <div class="contact-info">
+            <a href="https://wa.me/6282116079363"><span>Phone</span> +62 82 116 079 363</a>
+            <a href="mailto:sma28_garut@yahoo.com"><span>Email:</span> sma28_garut@yahoo.com</a>
+        </div>
+        <!-- Follow Us -->
+        <div class="follow-us">
+            <a href="{{ route('login') }}"><i class="fa fa-sign-in" aria-hidden="true"></i></a>
+            <a href="https://web.facebook.com/OSIS-SMAN-28-Garut-112020660339930"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+        </div>
+    </div>
+</footer>
